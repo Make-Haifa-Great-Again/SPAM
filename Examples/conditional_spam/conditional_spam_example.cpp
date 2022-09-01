64e362ad51e348e7bc27ef4148e829e05fa12a4e
Focault <96660268+Focault@users.noreply.github.com>
2022-09-01 15:54:53 +0300

    conditional spam example extension

--- Examples/conditional_spam/conditional_spam_example.cpp
+++ Examples/conditional_spam/conditional_spam_example.cpp
@@ -5,8 +5,10 @@ KITCHEN(CONDITIONAL_HOG, Condition);
 KITCHEN(Enum_HOG, Enum);
 KITCHEN(UNVALID_HOG, Unvalid);
 
+
 int main() 
 {
+    std::cout << "If a spam id has a negative value it's invalid in our internal logic" << "\n";
     std::cout << BON(Enum, toString)(A) << " = ";
     std::cout << A << '\n';
     std::cout << BON(Enum, toString)(B) << " = ";
@@ -16,8 +18,10 @@ int main()
     std::cout << BON(Enum, toString)(D) << " = ";
     std::cout << D << '\n';
     std::cout << "Is E Valid? - " << (BON(Enum, isValid)(static_cast<Enum>(E)) ? "true" : "false") << '\n';
-    std::cout << "Is 'E' Valid? - " << (BON(Enum, isStringValid)("E") ? "true" : "false") << '\n';
-    std::cout << "Is 'C' Valid? - " << (BON(Enum, isStringValid)("C") ? "true" : "false") << '\n';
+    std::cout << "Is 'E' an Enum spam? - " << (BON(Enum, isStringValid)("E") ? "true" : "false") << '\n';
+    std::cout << "Is 'C' an Enum spam? - " << (BON(Enum, isStringValid)("C") ? "true" : "false") << '\n';
+    std::cout << "Is C Valid? - " << ((C > static_cast<Enum>(0)) ? "true" : "false") << '\n';
+    std::cout << "Is SOMETHING_ELSE Valid? - " << ((SOMETHING_ELSE > static_cast<Enum>(0)) ? "true" : "false") << '\n';
     std::cout << "Is 3 Valid? - " << (BON(Enum, isValid)(static_cast<Enum>(3)) ? "true" : "false") << '\n';
     std::cout << "Is FAKE_B Valid? - " << (BON(Enum, isValid)(static_cast<Enum>(FAKE_B)) ? "true" : "false") << '\n';
     return 0;
